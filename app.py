3e05369418960e4cd048532d6cf8148fc7863165
Jared <jared_glenn@yahoo.com>
2023-10-06 02:39:54 -0600

    add login and secret route, protected secret route

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 """Routes for each page."""
 
-from flask import Flask, request, render_template, redirect, jsonify
+from flask import Flask, request, render_template, redirect, jsonify, session, flash
 from flask_debugtoolbar import DebugToolbarExtension
+from flask_bcrypt import Bcrypt
 from models import db, connect_db, User
-from forms import UserForm
+from forms import RegisterForm, LoginForm
 
 app = Flask(__name__)
 
@@ -18,6 +19,8 @@ connect_db(app)
 app.app_context().push()
 db.create_all()
 
+bcrypt = Bcrypt()
+
 
 # Main page
 
@@ -30,21 +33,66 @@ def home():
 
 # Registration page
 
-@app.route('/register')
+@app.route('/register', methods=["GET", "POST"])
 def register():
     """Register a new user."""
     
-    form = UserForm()
+    form = RegisterForm()
     
     if form.validate_on_submit():
         username = form.username.data
-        password = form.password.data
         email = form.email.data
         first_name = form.first_name.data
         last_name = form.last_name.data
+        
+        hashed = bcrypt.generate_password_hash(form.password.data)
+        password = hashed.decode("utf8")
         
         new_user = User(username=username, password=password, email=email, first_name=first_name, last_name=last_name)
         db.session.add(new_user)
         db.session.commit()
         
-    return render_template("register.html", form=form)
+        session["username"] = new_user.username
+        
+        return redirect('/secret')
+    else:
+        return render_template("register.html", form=form)
+
+# Login page
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    """Login to site."""
+    
+    form =LoginForm()
+    
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+        
+        user = User.query.filter_by(username=username).first()
+        
+        if user and bcrypt.check_password_hash(user.password, password):
+            
+            session["username"] = user.username
+            
+            return redirect('/secret')
+        
+        else:
+            form.username.errors = ["Invalid username or password."]
+            
+    return render_template("login.html", form=form)
+
+
+# Secret page
+
+@app.route('/secret', methods=["GET"])
+def secret():
+    """Secret page."""
+    
+    if "username" not in session:
+        flash("Please log in first!")
+        
+        return redirect('/login')
+    
+    return render_template("secret.html")
--- forms.py
+++ forms.py
@@ -1,13 +1,13 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, FloatField
+from wtforms import StringField, PasswordField, FloatField
 from wtforms.validators import InputRequired, Optional, AnyOf, URL, NumberRange, Length
 
-class UserForm(FlaskForm):
+class RegisterForm(FlaskForm):
     """Form for adding a new user."""
     
     username = StringField("Username",
                            validators=[Length(max=20, message="Username maximum length is 20 characters.")])
-    password = StringField("Password")
+    password = PasswordField("Password")
     email = StringField("Email",
                         validators=[InputRequired(), Length(max=50, message="Email maximum length is 50 characters.")])
     first_name = StringField("First Name",
@@ -15,4 +15,11 @@ class UserForm(FlaskForm):
     last_name = StringField("Last Name",
                             validators=[InputRequired(), Length(max=30, message="Name maximum length is 30 characters.")])
     
+    
+class LoginForm(FlaskForm):
+    """Form for adding a new user."""
+    
+    username = StringField("Username",
+                           validators=[Length(max=20, message="Username maximum length is 20 characters.")])
+    password = PasswordField("Password")
     
